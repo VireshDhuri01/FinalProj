cff845d17c52a2f261d22860a170a2fed900487f
VireshDhuri01 <vireshdhuri28@gmail.com>
2026-06-18 14:58:31 +0530

    Update vpc.tf

--- terraform_code/eks_code/vpc.tf
+++ terraform_code/eks_code/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.13.0"
+  version = "~> 5.0"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -11,6 +11,7 @@ module "vpc" {
   intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
+  single_nat_gateway = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
